62a28f9f18dbb87ecaca4736a5391c64d3acad74
Vladimir Molchanov <r1ken@mail.ru>
2024-08-09 22:30:55 +0300

    fix items

--- pep_parse/items.py
+++ pep_parse/items.py
@@ -7,12 +7,6 @@ import scrapy
 
 
 class PepParseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
-
-
-class PepParseItem(scrapy.Item):
     number = scrapy.Field()
     name = scrapy.Field()
     status = scrapy.Field()
