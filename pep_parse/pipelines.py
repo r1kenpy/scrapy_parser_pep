d4f8f99184de1a500dc9d7679a957807c7952ce5
Vladimir Molchanov <r1ken@mail.ru>
2024-08-10 18:42:55 +0300

    fix

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -2,12 +2,37 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import csv
+import os
+from collections import defaultdict
+from datetime import datetime as dt
+from pathlib import Path
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from pep_parse.settings import BASE_DIR, RESULTS_DIR
+
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        self.number_statuses = defaultdict(int)
+        self.result_dir = BASE_DIR / RESULTS_DIR
+        self.result_dir.mkdir(exist_ok=True)
+
     def process_item(self, item, spider):
+        self.number_statuses[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        with open(
+            self.result_dir
+            / f'status_summary_{dt.now().strftime("%d-%m-%Y_%H:%M:%S")}.csv',
+            'w',
+            encoding='utf-8',
+        ) as f:
+            fieldnames = ('status', 'quantity')
+            w = csv.DictWriter(f, dialect=csv.excel, fieldnames=fieldnames)
+            w.writeheader()
+            for k, v in self.number_statuses.items():
+                w.writerow({fieldnames[0]: k, fieldnames[1]: v})
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -8,7 +8,7 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
         for pep in response.css(
